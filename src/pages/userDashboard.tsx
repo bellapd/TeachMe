b497319a124a1cccec2b7f4c2804b326dbf88e48
Jet <bladezjt@gmail.com>
2023-06-14 22:49:51 +0800

    14062023 Update Navbar Animation and init user Dashboard

---
+++ src/pages/userDashboard.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import UserDash from "../components/userDash"
+function userDashboard() {
+  return (
+    <>
+    <Navbar/>
+    <UserDash/>
+    </>
+
+  );
+}
+
+export default userDashboard;
