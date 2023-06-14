5e15c1072e0391b3128bf4d6c20b4df7d1262d75
Bella <annabellaputridirgo@gapp.nthu.edu.tw>
2023-06-14 21:10:05 +0800

    sigin dashboard 230614

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import NavbarRegister from "@/components/NavbarRegister";
+import Jumbotron from "@/components/Jumbotron";
+import About from "@/components/About";
+import Howtouse from "@/components/Howtouse";
 
 function dashboard() {
   return (
     <div>
       <NavbarRegister />
+      <Jumbotron />
+      <About />
+      <Howtouse />
     </div>
   );
 }
