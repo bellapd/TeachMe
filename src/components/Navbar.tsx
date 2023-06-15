7ce6046b58dcacd5737f5500f8c3129af8a58600
jansenchristophery <128026898+jansenchristophery@users.noreply.github.com>
2023-06-15 15:58:21 +0800

    Appointment 230615

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,8 +32,7 @@ export default function Example() {
         </a>
       </Typography>
       <Typography variant="small" color="blue-gray" className="font-normal">
-        <a href="/profiledetails" className="flex items-center focus:outline-none">
-        // <a href="#" className="flex items-center focus:outline-none scale-100 hover:scale-150 ease-in duration-200">
+        <a href="communities" className="flex items-center focus:outline-none scale-100 hover:scale-150 ease-in duration-200">
           Community
         </a>
       </Typography>
