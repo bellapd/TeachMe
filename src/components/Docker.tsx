bb04a444b21b2ef8913ab8e5d9d2744436573cc6
Bella <annabellaputridirgo@gapp.nthu.edu.tw>
2023-06-17 18:28:01 +0800

    170623 Docker Profile DONE

--- src/components/Docker.tsx
+++ src/components/Docker.tsx
@@ -17,8 +17,7 @@ export default function Example() {
   const [activeCard, setActiveCard] = useState(0);
   const cards = [
     {
-      image:
-        "/images/docker1.svg",
+      image: "/images/docker1.svg",
       title: "Croy Karz",
       subtitle: "Docker Teacher with 99 years of experience",
       description:
@@ -26,23 +25,21 @@ export default function Example() {
       link: "/appointment/croykarz",
     },
     {
-        image:
-          "/images/docker2.svg",
-        title: "Hendra",
-        subtitle: "Docker Teacher with 99 years of experience",
-        description:
-          "I have studied Docker for the longest time. I have also used it in almost of my project, be it a huge collaborative project with big company, or personal side hustle. This mean I have a wide understanding of how to use Docker to its fullest.",
-        link: "/appointment/hendra",
-      },
-      {
-        image:
-          "/images/docker3.svg",
-        title: "Agus",
-        subtitle: "Docker Teacher with 99 years of experience",
-        description:
-          "I have studied Docker for the longest time. I have also used it in almost of my project, be it a huge collaborative project with big company, or personal side hustle. This mean I have a wide understanding of how to use Docker to its fullest.",
-        link: "/appointment/agus",
-      },
+      image: "/images/docker2.svg",
+      title: "Hendra",
+      subtitle: "Docker Teacher with 99 years of experience",
+      description:
+        "I have studied Docker for the longest time. I have also used it in almost of my project, be it a huge collaborative project with big company, or personal side hustle. This mean I have a wide understanding of how to use Docker to its fullest.",
+      link: "/appointment/hendra",
+    },
+    {
+      image: "/images/docker3.svg",
+      title: "Agus",
+      subtitle: "Docker Teacher with 99 years of experience",
+      description:
+        "I have studied Docker for the longest time. I have also used it in almost of my project, be it a huge collaborative project with big company, or personal side hustle. This mean I have a wide understanding of how to use Docker to its fullest.",
+      link: "/appointment/agus",
+    },
     // Add more card objects as needed
   ];
 
@@ -56,18 +53,14 @@ export default function Example() {
         <div className="relative h-full">
           <div className="text-black py-12 px-64 flex items-center">
             <Image
-                src="/images/docker.svg"
-                width={81}
-                height={50}
-                alt="docker_logo"
-                className="object-cover"
-            /> 
-            <Typography
-              variant="h2"
-              color="blue-gray"
-              className="mb-2 px-2"
-            >
-              Python
+              src="/images/docker.svg"
+              width={81}
+              height={50}
+              alt="docker_logo"
+              className="object-cover"
+            />
+            <Typography variant="h2" color="blue-gray" className="mb-2 px-2">
+              Docker
             </Typography>
           </div>
           <Carousel
@@ -82,16 +75,17 @@ export default function Example() {
               return (
                 <div
                   className={`${
-                    hasPrev ? 'absolute' : 'hidden'
+                    hasPrev ? "absolute" : "hidden"
                   } top-0 bottom-0 left-0 flex justify-center items-center p-10 opacity-50 hover:opacity-100 cursor-pointer z-20`}
                   onClick={clickHandler}
                 >
-                  <Image 
+                  <Image
                     src="/images/leftArrow.svg"
                     width={100}
                     height={100}
-                    alt = "leftArrow"
-                    className="w-20 h-20 text-white" />
+                    alt="leftArrow"
+                    className="w-20 h-20 text-white"
+                  />
                 </div>
               );
             }}
@@ -99,67 +93,76 @@ export default function Example() {
               return (
                 <div
                   className={`${
-                    hasNext ? 'absolute' : 'hidden'
+                    hasNext ? "absolute" : "hidden"
                   } top-0 bottom-0 right-0 flex justify-center items-center p-10 opacity-50 hover:opacity-100 cursor-pointer z-20`}
                   onClick={clickHandler}
                 >
-                  <Image 
+                  <Image
                     src="/images/rightArrow.svg"
                     width={100}
                     height={100}
-                    alt = "rightArrow"
-                    className="w-20 h-20 text-white" />
+                    alt="rightArrow"
+                    className="w-20 h-20 text-white"
+                  />
                 </div>
               );
             }}
           >
             {cards.map((card, index) => (
-            <div key={index} className="carousel-card flex-col items-center px-64">
-              <Card className="bg-purple-200">
-                <CardBody className="flex">
-                  <div>
-                    <Image
-                      src={card.image}
-                      width={50}
-                      height={50}
-                      alt="image"
-                      className="w-2/5  m-0 rounded-r-none object-cover"
-                    />
-                  </div>
-                  <div>
-                    <Typography
-                      variant="h6"
-                      color="blue"
-                      className="uppercase mb-4"
+              <div
+                key={index}
+                className="carousel-card flex-col items-center px-64"
+              >
+                <Card className="bg-purple-200">
+                  <CardBody className="flex">
+                    <div>
+                      <Image
+                        src={card.image}
+                        width={50}
+                        height={50}
+                        alt="image"
+                        className="w-2/5  m-0 rounded-r-none object-cover"
+                      />
+                    </div>
+                    <div>
+                      <Typography
+                        variant="h6"
+                        color="blue"
+                        className="uppercase mb-4"
+                      >
+                        {card.title}
+                      </Typography>
+                      <Typography
+                        variant="h4"
+                        color="blue-gray"
+                        className="mb-2"
+                      >
+                        {card.subtitle}
+                      </Typography>
+                      <Typography color="gray" className="font-normal mb-8">
+                        {card.description}
+                      </Typography>
+                    </div>
+                  </CardBody>
+                  <div className="flex-col items-center">
+                    <a
+                      href={user && clerkUser ? card.link : "/signin"}
+                      className="inline-flex justify-center items-center mt-4 py-3 px-5 text-base font-large text-center text-[#4700C6] rounded-xl bg-[#FFE873]"
                     >
-                      {card.title}
-                    </Typography>
-                    <Typography
-                      variant="h4"
-                      color="blue-gray"
-                      className="mb-2"
-                    >
-                      {card.subtitle}
-                    </Typography>
-                    <Typography color="gray" className="font-normal mb-8">
-                      {card.description}
-                    </Typography>
+                      <Button
+                        variant="text"
+                        className="flex items-center gap-2 text-center"
+                      >
+                        Make an Appointment
+                      </Button>
+                    </a>
                   </div>
-                </CardBody>
-                <div className="flex-col items-center">
-                  <a href={ user&&clerkUser ? card.link : "/signin"} className="inline-flex justify-center items-center mt-4 py-3 px-5 text-base font-large text-center text-[#4700C6] rounded-xl bg-[#FFE873]">
-                    <Button variant="text" className="flex items-center gap-2 text-center">
-                      Make an Appointment
-                    </Button>
-                  </a>
-                </div>                
-              </Card>
-            </div>
+                </Card>
+              </div>
             ))}
           </Carousel>
         </div>
       </section>
-      
     </div>
   );
 }
