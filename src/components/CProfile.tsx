c1013f0737e1ba25568a6ee6fd0d2bdec40b325c
Bella <annabellaputridirgo@gapp.nthu.edu.tw>
2023-06-17 12:27:36 +0800

    170623 C and Python view course, Mobile Friendly

--- src/components/CProfile.tsx
+++ src/components/CProfile.tsx
@@ -10,10 +10,10 @@ function Profile() {
           className="float-left bg-purple-200 p-6 w-1/5 h-screen  flex flex-col justify-center items-center text-black"
         >
           <Image
-            className="rounded-none"
-            src="/images/picprofile.svg"
-            width={240}
-            height={240}
+            className="rounded-full"
+            src="/images/cteacher.svg"
+            width={200}
+            height={5}
             alt="profilepic"
           />
           <br />
@@ -22,7 +22,7 @@ function Profile() {
             id="ProfileName"
             className="font-serif font-bold text-3xl text-center capitalize text-purple-800"
           >
-            Budi Sumaker
+            Frans
           </p>
           <br />
           <div className=" inline-block">
@@ -32,19 +32,17 @@ function Profile() {
               width={17}
               height={24}
             ></img>
-            <p className=" inline-block"> &nbsp; England</p>
+            <p className=" inline-block"> &nbsp; Dutch</p>
           </div>
           <div className=" inline-block">
+            <br />
             <img
               src="/images/message.svg"
               className=" inline-block"
               width={17}
               height={24}
             ></img>
-            <p className=" inline-block">
-              {" "}
-              &nbsp; English, Mandarin, Ungabunga
-            </p>
+            <p className=" inline-block"> &nbsp; English, Dutch, German</p>
           </div>
           <div className=" inline-block">
             <img
@@ -53,7 +51,7 @@ function Profile() {
               width={17}
               height={24}
             ></img>
-            <p className=" inline-block"> &nbsp; Hogwart School of Tech</p>
+            <p className=" inline-block"> &nbsp; NTU, Taiwan</p>
           </div>
           <br />
           <br />
@@ -71,14 +69,14 @@ function Profile() {
           <br />
 
           <h3 className="font-bold text-left font-sans ">
-            Python programmer with 99 years of experience
+            C programmer with 99 years of experience
           </h3>
           <hr className="border-6  border-black" />
           <br />
           <p className=" font-sans text-mg">
-            I have studied Python for the longest time. I have also used it in
-            almost all of my projects, be it a huge collaborative project with a
-            big company or a personal side hustle. This means I have a wide
+            I have studied C for the longest time. I have also used it in almost
+            all of my projects, be it a huge collaborative project with a big
+            company or a personal side hustle. This means I have a wide
             understanding of how to use Python to its fullest.
           </p>
 
@@ -89,33 +87,40 @@ function Profile() {
           <div className=" px-6">
             <ul className="list-disc font-sans font-semibold ">
               <p className="font-sans text-mg font-semibold">
-                Module 1: Introduction to Python
+                Module 1: Introduction to C Programming and Environment Setup
               </p>
-              <li>Installation and setup</li>
-              <li>Basic syntax and variables</li>
-              <li>Input and output operations</li>
+              <li>Course Introduction and Objectives</li>
+              <li>Overview of the C programming language</li>
+              <li>
+                Setting up a C development environment (compiler, IDE, and text
+                editor)
+              </li>
+              <li>Writing and running your first C program</li>
               <br />
               <p className="font-sans text-mg font-semibold">
-                Module 2: Control Flow and Looping
+                Module 2: Basic Syntax and Data Types
               </p>
-              <li>Conditional statements</li>
-              <li>Looping structures</li>
-              <li>Logical operators</li>
+              <li>C syntax and structure</li>
+              <li>Variables and data types (int, float, char)</li>
+              <li>Constants and literals</li>
+              <li>Input and output operations (printf and scanf)</li>
               <br />
               <p className="font-sans text-mg font-semibold">
-                Module 3: Data Structures
+                Module 3: Control Structures
               </p>
-              <li>Lists</li>
-              <li>Tuples</li>
-              <li>Dictionaries</li>
-              <li>String manipulation</li>
+              <li>
+                Decision-making with if, if-else, and nested if statements
+              </li>
+              <li>Looping structures: while, do-while, and for loops</li>
+              <li>Switch case statements</li>
+              <li>Break and continue statements</li>
               <br />
 
               <p className="font-sans text-mg font-semibold">
-                Module 4: File Handling
+                Module 4: Pointers
               </p>
-              <li>Reading and writing files</li>
-              <li>Error handling</li>
+              <li>Pointer declaration and initialization</li>
+              <li>Pointer and arrays</li>
             </ul>
           </div>
         </article>
--- src/components/userDash.tsx
+++ src/components/userDash.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Image from "next/image";
 import { Button } from "@material-tailwind/react";
-import 'font-awesome/css/font-awesome.min.css';
-import { NextResponse } from 'next/server';
-import { useUser } from '@clerk/nextjs';
+import "font-awesome/css/font-awesome.min.css";
+import { NextResponse } from "next/server";
+import { useUser } from "@clerk/nextjs";
+import Link from "next/link";
 
 export default function UserDashboardSHEEESH() {
   const { user } = useUser();
@@ -11,19 +12,33 @@ export default function UserDashboardSHEEESH() {
 
   return (
     <div className="h-screen justify-center items-center bg-white">
-      <div id="profile" className="pb-10 bg-purple-100 flex flex-col justify-center items-center relative py-5">
+      <div
+        id="profile"
+        className="pb-10 bg-purple-100 flex flex-col justify-center items-center relative py-5"
+      >
         <div className="rounded-full bg-black w-40 h-40 flex items-center justify-center py-5">
-          <img src={user?.profileImageUrl} className="rounded-full w-40 h-40" alt="Profile Image" />
+          <img
+            src={user?.profileImageUrl}
+            className="rounded-full w-40 h-40"
+            alt="Profile Image"
+          />
         </div>
         <p className="text-2xl text-black font-bold">
-          {user?.firstName} <a href="https://facebook.com"><i className="fa fa-share"></i></a>
+          {user?.firstName}{" "}
+          <a href="https://facebook.com">
+            <i className="fa fa-share"></i>
+          </a>
         </p>
       </div>
       <div className="h-1/2 bg-white flex overflow-x-auto py-24">
-        <p className="text-purple-500 text-3xl font-bold mx-10 my-10">My Courses</p>
+        <p className="text-purple-500 text-3xl font-bold mx-10 my-10">
+          My Courses
+        </p>
 
         {cour.length === 0 ? (
-          <p className="py-11 text-2xl text-gray-500">You are not taking any course right now</p>
+          <p className="py-11 text-2xl text-gray-500">
+            You are not taking any course right now
+          </p>
         ) : (
           cour.map((course, index) => (
             <div
@@ -32,13 +47,20 @@ export default function UserDashboardSHEEESH() {
               className="bg-purple-200 w-[500px] h-[200px] rounded-3xl flex-shrink-0 mx-5 flex"
             >
               <div className="flex-1 flex items-center justify-center">
-                <Image src={`/images/${course}.svg`} width={150} height={150} alt={course} />
+                <Image
+                  src={`/images/${course}.svg`}
+                  width={150}
+                  height={150}
+                  alt={course}
+                />
               </div>
               <div className="flex flex-col justify-center items-center p-5">
                 <p className="text-3xl text-black mb-5">{course}</p>
                 <a>
                   <Button>
-                    <p>View Course</p>
+                    <Link href={`/teacher-profile/${course}`}>
+                      <p>View Course</p>
+                    </Link>
                   </Button>
                 </a>
               </div>
